4d87900af03ffe107dcfb6c4e4df5529e2509a17
Oleg_B <100700204+AntonKnuth@users.noreply.github.com>
2025-10-10 12:06:04 +0200

    Experimented with both rows

--- write-ard/LCD Display/writeDisplay.ino/writeDisplay.ino.ino
+++ write-ard/LCD Display/writeDisplay.ino/writeDisplay.ino.ino
@@ -6,27 +6,37 @@
 
 int counter =0 ;
 
-LiquidCrystal_PCF8574 display(LCD_ADDRESS);
+
 
 #define LCD_ROWS 2
 #define LCD_COLUMNS 16
-#define SCOLL_DELAY 150
+#define SCROLL_DELAY 150
 #define BACKLIGHT 255
+
+LiquidCrystal_PCF8574 display(LCD_ADDRESS);
 
 void setup() {
   Serial.begin(9600);
   while(!Serial);
 
   display.begin(LCD_COLUMNS, LCD_ROWS);
+  display.setBacklight(BACKLIGHT);
 
 }
 
 void loop() {
   display.clear();
-  if(counter%2==0)
+  if(counter%2==0){
+    //Setze den Cursor auf die obere Reihe
+    display.setCursor(0,0);
     display.print("Moin!");
-  else
-    display.print("Hallo!"); 
+   
+  }
+  else{
+    //Setze den Cursor auf die untere Reihe
+    display.setCursor(0,1);
+    display.print("Hallo!");
+  } 
     counter++;
   delay(4000);
 

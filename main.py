24b94bb614a7bfdbdaf3fe1645b9f71834233b4b
Oleg_B <100700204+AntonKnuth@users.noreply.github.com>
2025-10-13 13:34:42 +0200

    + data reader for humidity and temperature data

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from modules import voice_recognition
+from modules import insert
 
 def main():
     try:
         mic_stream, wake_word_detector, speech_recognizer = voice_recognition.setup_recognize_speech()
-
+        
+        dht_stream = insert
+        
         while True:
             print(voice_recognition.recognize_speech(mic_stream, wake_word_detector, speech_recognizer))
+            dht_stream.read_Temp_and_Humid_data()
     
     except Exception as e:
         print(f"Error: {e}")
--- modules/insert.py
+++ modules/insert.py
@@ -4,17 +4,17 @@ import time
 import datetime
 time.sleep(2)
 
-with serial.Serial('/dev/ttyACM0',9600,timeout=1) as ser:
-	try:
-		while True:
-			if ser.in_waiting > 0:
-				data = ser.readline().decode('utf-8').strip()
-				temp, humid = data.split(',')
-				temp = float(temp)
-				humid = float(humid)
-				now = datetime.datetime.now()
-				print(f"{data}")
-				database.insert_sensor_data([(now, "Temperatur", temp),(now, "Luftfeuchigkeit", humid)])
-	except KeyboardInterrupt:
-		print("Exiting")
-
+def read_Temp_and_Humid_data():
+	with serial.Serial('/dev/ttyACM0',9600,timeout=1) as ser:
+		try:
+			while True:
+				if ser.in_waiting > 0:
+					data = ser.readline().decode('utf-8').strip()
+					temp, humid = data.split(',')
+					temp = float(temp)
+					humid = float(humid)
+					now = datetime.datetime.now()
+					print(f"{data}")
+					database.insert_sensor_data([(now, "Temperatur", temp),(now, "Luftfeuchigkeit", humid)])
+		except KeyboardInterrupt:
+			print("Exiting")
